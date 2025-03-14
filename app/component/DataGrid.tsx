649b11761ecebd863034f857a35f6d1e62383141
Aditya Kumar <neelaadityakumar@gmail.com>
2025-03-14 15:26:52 +0530

    add icon

--- app/component/DataGrid.tsx
+++ app/component/DataGrid.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import Table from "./Table";
 import { DataRow } from "../types";
+import DownloadIcon from "../icons/Download";
 
 interface DataGridProps {
   data: DataRow[];
@@ -76,22 +77,11 @@ const DataGrid: React.FC<DataGridProps> = ({ data }) => {
             disabled={!canDownload}
             onClick={handleDownload}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
+            <DownloadIcon
               className={`h-6 w-6 ${
                 !canDownload ? "opacity-50" : "opacity-100"
               }`}
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 16v2a2 2 0 002 2h12a2 2 0 002-2v-2m-4-4l-4 4m0 0l-4-4m4 4V4"
-              />
-            </svg>
+            />
             Download Selected
           </button>
         </div>
---
+++ app/icons/Download.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const DownloadIcon = ({ className = "" }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4 16v2a2 2 0 002 2h12a2 2 0 002-2v-2m-4-4l-4 4m0 0l-4-4m4 4V4"
+      />
+    </svg>
+  );
+};
+
+export default DownloadIcon;
